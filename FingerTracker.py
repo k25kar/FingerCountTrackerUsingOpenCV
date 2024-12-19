70a84566a1a73d9c146d98221a7f60a2fd192e1e
Karthik Suryanarayanan <s.karthik.suryanarayanan@gmail.com>
2024-12-19 15:18:02 +0530

    Add files via upload

---
+++ FingerTracker.py
@@ -0,0 +1,73 @@
+import cv2
+import time
+import HandTrackingModule as htm
+
+# Camera dimensions
+wCam, hCam = 640, 480
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+# Hand detector
+detector = htm.handDetector(detectionCon=0.75)
+
+# Finger tip landmarks
+tipIds = [4, 8, 12, 16, 20]
+
+# Variables for FPS calculation
+pTime = 0
+
+while True:
+    # Capture frame from camera
+    success, img = cap.read()
+    if not success:
+        break
+
+    # Detect hands in the frame
+    img = detector.findHands(img)
+
+    # Get landmark positions
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Other fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # Count raised fingers
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        # Display the finger count on the screen
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                    10, (255, 0, 0), 25)
+
+    # Calculate and display FPS
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+
+    # Show the live feed
+    cv2.imshow("Image", img)
+
+    # Exit when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
